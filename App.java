11d290792a7fd31706b6a15c1bd16eea4765739a
bagii <gabrielyyrosario@gmail.com>
2026-04-23 11:01:47 -0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,34 +1,49 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner teclado = new Scanner (System.in);
+        //declaração de váriaveis:
+        int anoNasc, idade, aposenta; //variável do ano de nascimento - mumero intero
+        double renda, novaRenda; //variável de rend - numero real
+        boolean dev; // variável lógica se vc é programador: true ou false
+        String nome; // pergunta o nome do programador
+        char genero; //define o genero da pessoa em uma letra só
+        Scanner teclado = new Scanner(System.in); //instalar entrada
+        PrintStream video = new PrintStream(System.out);
 
-        double b1, b2, b3, b4, nf;
-        String seraquedeuboa; 
-        System.out.println("Informe as 4 notas bimestrais 1b 2b 3b 4b :");
-        b1 = teclado.nextDouble();
-        b2 = teclado.nextDouble();
-        b3 = teclado.nextDouble();
-        b4 = teclado.nextDouble();
+        //entrada das váriaveis:
+        video.println("Calculadora de idade!");
+        video.println("Qual seu nome?");
+        nome = teclado.next(); //faz leitura da string nome
+        video.println("Você é programador?");
+        dev = teclado.nextBoolean(); 
+        video.println("Qual seu genero? (letra)");
+        genero = teclado.next().charAt(0); //forma adequada de ler um caractere
+        video.println("Qual sua renda mensal?");
+        renda = teclado.nextDouble();
+        video.println("Em que ano nasceu?");
+        anoNasc = teclado.nextInt();
 
-        nf = (b1+b2+b3+b4)/4;
-        seraquedeuboa = "Reprovado";
-
-        if(nf >= 6.0){
-            seraquedeuboa = "Aprovado";
-
-        } 
-        else if (nf >=4 & nf < 5.9){
-            seraquedeuboa = "Recuperação!";
-        } 
-        else{
-            seraquedeuboa = "Reprovado!";
+        //procedimentos lógicos e aritméticos:
+        idade = 2026 - anoNasc; //diz idade completa esse ano
+        renda = renda*12;//renda anual total básica com déimo teceiro:
+        if(genero=='m'){ //teste para ver daqui quanto tempo aposenta
+            aposenta=70-idade; //daqui quanto tempo aposenta se masc
+        }
+        else {
+         aposenta=65 - idade;
         }
 
+        if(dev){
+            novaRenda=renda*3/13;
+        } else novaRenda=renda/13;
 
-        System.out.println("Sua média final foi de: " + nf );
-        System.out.println(seraquedeuboa);
-
+        //procedimentos de saída
+        video.println(nome + " este ano completa " + idade + " anos. ");
+        video.print("Sua renda anual é de R$" + renda);
+        video.println("Faltam " + aposenta + " anos para se aposentar! ");
+        video.println("DEV seria com renda mensal de R$" + novaRenda);
+        video.println("Se fosse DEV para gringa o sálario é de R$" + novaRenda*5);
     }
 }
